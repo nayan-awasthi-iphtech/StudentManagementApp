c72ababded16caa62e4327f16fab59ae14d80949
nayan-awasthi-iphtech <nayan.awasthi@iphtechnologies.com>
2026-08-25 10:20:30 +0530

    Initial Commit

---
+++ StudentManagementApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentManagementApp
+//
+//  Created by iPHTech 30 on 25/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
